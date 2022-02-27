5fabf5f7909f774aaa8f9c75c77acc3bc0924f23
jonhayjr <jonhayjr@gmail.com>
2022-02-26 16:28:40 -0800

    Changed input element to type number

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 /*Array of possible password characters*/
-const possibleCharacters = ["1","2","3","4","5","6","7","8","9","10", "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "?", "!", "@", "<", ">", ":", ";", "&", "=", "+", "-"]
+const possibleCharacters = ["1","2","3","4","5","6","7","8","9", "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "?", "!", "@", "<", ">", ":", ";", "&", "=", "+", "-"]
 
 /*Function to get random number*/
 const getRandomNumber = (max) => {
@@ -7,11 +7,11 @@ const getRandomNumber = (max) => {
 }
 
 /*Function to get random password*/
-const getRandomPassword = (length = 12) => {
+const getRandomPassword = (length) => {
     let password = '';
 
-    for (let i=1; i <= 12; i++) {
-        let randomNumber = getRandomNumber(possibleCharacters.length);
+    for (let i=1; i <= length; i++) {
+        const randomNumber = getRandomNumber(possibleCharacters.length);
         password += possibleCharacters[randomNumber];
     }
     return password;
@@ -19,15 +19,19 @@ const getRandomPassword = (length = 12) => {
 
 /*Function to generate 4 random passwords and update input values*/
 const generatePasswords = () => {
-    let password1El = document.getElementById('password1');
-    let password2El = document.getElementById('password2');
-    let password3El = document.getElementById('password3');
-    let password4El = document.getElementById('password4');
+    const passwordLengthEl = document.getElementById('password-length-input');
+    const passwordLengthElValue = passwordLengthEl.value;
+    let passwordLength = passwordLengthElValue;
+  
+    const password1El = document.getElementById('password1');
+    const password2El = document.getElementById('password2');
+    const password3El = document.getElementById('password3');
+    const password4El = document.getElementById('password4');
 
-    let randomPassword1 = getRandomPassword();
-    let randomPassword2 = getRandomPassword();
-    let randomPassword3 = getRandomPassword();
-    let randomPassword4 = getRandomPassword();
+    const randomPassword1 = getRandomPassword( passwordLength);
+    const randomPassword2 = getRandomPassword( passwordLength);
+    const randomPassword3 = getRandomPassword (passwordLength);
+    const randomPassword4 = getRandomPassword( passwordLength);
 
     password1El.value = randomPassword1;
     password2El.value = randomPassword2;
@@ -38,3 +42,4 @@ const generatePasswords = () => {
 /*Adds event listener to Generate Passwords button and runs generatePasswords function*/
 const generatePasswordBtn = document.getElementById('password-generator-btn');
 generatePasswordBtn.addEventListener('click', generatePasswords);
+
